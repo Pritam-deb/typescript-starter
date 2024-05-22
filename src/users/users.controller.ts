9d2e1b7368727091f3ca7219fa20a5771610af22
Pritam-debnath <pritamd@geekyants.com>
2024-05-22 17:46:05 +0530

    established CRUD routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll(@Query('role') role?: 'admin' | 'intern' | 'mentor') {
+    if (role === 'admin') {
+      return [`all admins`];
+    }
+    return [];
+  }
+
+  @Get('interns')
+  findAllInterns() {
+    return ['all interns'];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: object) {
+    console.log('Post request');
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
